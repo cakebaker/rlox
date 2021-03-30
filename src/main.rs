7d426afb954b6387ae246e472a188face040d7ab
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-03-30 16:42:23 +0200

    Implement main() & handle args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => {
+            println!("Usage: rlow [script]");
+            // exit code from https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+            std::process::exit(64);
+        }
+    }
+}
+
+fn run_prompt() {
+    // do something
+}
+
+fn run_file(path: &str) {
+    // do something
 }
