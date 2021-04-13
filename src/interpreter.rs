fd997b230582926abe76d18fb064a465a138026d
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-04-13 15:19:24 +0200

    Interpret unary expressions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,5 +1,7 @@
 use crate::expr::Expr;
 use crate::expr::Literal;
+use crate::token::Token;
+use crate::token_type::TokenType;
 
 #[derive(Debug)]
 pub struct RuntimeError {}
@@ -20,6 +22,27 @@ impl Interpreter {
         match expr {
             Expr::Literal(literal) => Ok(literal),
             Expr::Grouping { expression: expr } => Self::evaluate(*expr),
+            Expr::Unary { operator, right } => Self::evaluate_unary(&operator, *right),
+            _ => Err(RuntimeError {}),
+        }
+    }
+
+    fn evaluate_unary(operator: &Token, right: Expr) -> Result<Literal, RuntimeError> {
+        let result = Self::evaluate(right)?;
+
+        match operator.token_type {
+            TokenType::Minus => match result {
+                Literal::Number(number) => Ok(Literal::Number(-number)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenType::Bang => {
+                // false and nil are treated as falsey, everything else is truthy
+                match result {
+                    Literal::Bool(bool) => Ok(Literal::Bool(!bool)),
+                    Literal::Nil => Ok(Literal::Bool(true)),
+                    _ => Ok(Literal::Bool(false)),
+                }
+            }
             _ => Err(RuntimeError {}),
         }
     }
@@ -30,6 +53,8 @@ mod tests {
     use super::Interpreter;
     use crate::expr::Expr;
     use crate::expr::Literal;
+    use crate::token::Token;
+    use crate::token_type::TokenType;
 
     #[test]
     fn evaluate_literals() {
@@ -62,6 +87,44 @@ mod tests {
             assert_eq!(literal, result);
         } else {
             panic!("Interpreter::evaluate() returned unexpected Err");
+        }
+    }
+
+    #[test]
+    fn evaluate_negation() {
+        let expr = Expr::Unary {
+            operator: Token::new(TokenType::Minus, "-".to_string(), 1),
+            right: Box::new(Expr::Literal(Literal::Number(1.0))),
+        };
+
+        if let Ok(result) = Interpreter::evaluate(expr) {
+            assert_eq!(Literal::Number(-1.0), result);
+        } else {
+            panic!("Interpreter::evaluate() returned unexpected Err");
+        }
+    }
+
+    #[test]
+    fn evaluate_logical_not() {
+        let literals = vec![
+            (Literal::Nil, Literal::Bool(true)),
+            (Literal::Bool(false), Literal::Bool(true)),
+            (Literal::Bool(true), Literal::Bool(false)),
+            (Literal::Number(1.0), Literal::Bool(false)),
+            (Literal::String("str".to_string()), Literal::Bool(false)),
+        ];
+
+        for (literal, expected) in literals {
+            let expr = Expr::Unary {
+                operator: Token::new(TokenType::Bang, "!".to_string(), 1),
+                right: Box::new(Expr::Literal(literal)),
+            };
+
+            if let Ok(result) = Interpreter::evaluate(expr) {
+                assert_eq!(expected, result);
+            } else {
+                panic!("Interpreter::evaluate() returned unexpected Err");
+            }
         }
     }
 }
