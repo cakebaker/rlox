fc9b4a5202bf5e062359ed6591c5e39c8952bd3f
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-05-19 10:41:02 +0200

    Implement fmt::Display trait for TokenType

--- src/token_type.rs
+++ src/token_type.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum TokenType {
     // Single-character tokens
@@ -24,7 +26,7 @@ pub enum TokenType {
     LessEqual,
 
     // Literals
-    Identifier,
+    Identifier, // TODO should be Identifier(String)
     String(String),
     Number(f64),
 
@@ -47,4 +49,50 @@ pub enum TokenType {
     While,
 
     Eof,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::LeftParen => write!(f, "("),
+            Self::RightParen => write!(f, ")"),
+            Self::LeftBrace => write!(f, "{{"), // https://doc.rust-lang.org/std/fmt/index.html#escaping
+            Self::RightBrace => write!(f, "}}"), // https://doc.rust-lang.org/std/fmt/index.html#escaping
+            Self::Comma => write!(f, ","),
+            Self::Dot => write!(f, "."),
+            Self::Minus => write!(f, "-"),
+            Self::Plus => write!(f, "+"),
+            Self::Semicolon => write!(f, ";"),
+            Self::Slash => write!(f, "/"),
+            Self::Star => write!(f, "*"),
+            Self::Bang => write!(f, "!"),
+            Self::BangEqual => write!(f, "!="),
+            Self::Equal => write!(f, "="),
+            Self::EqualEqual => write!(f, "=="),
+            Self::Greater => write!(f, ">"),
+            Self::GreaterEqual => write!(f, ">="),
+            Self::Less => write!(f, "<"),
+            Self::LessEqual => write!(f, "<="),
+            Self::Identifier => write!(f, "identifier"), // TODO fix, see other todo above
+            Self::String(s) => write!(f, "{}", s),
+            Self::Number(n) => write!(f, "{}", n),
+            Self::And => write!(f, "and"),
+            Self::Class => write!(f, "class"),
+            Self::Else => write!(f, "else"),
+            Self::False => write!(f, "false"),
+            Self::Fun => write!(f, "fun"),
+            Self::For => write!(f, "for"),
+            Self::If => write!(f, "if"),
+            Self::Nil => write!(f, "nil"),
+            Self::Or => write!(f, "or"),
+            Self::Print => write!(f, "print"),
+            Self::Return => write!(f, "return"),
+            Self::Super => write!(f, "super"),
+            Self::This => write!(f, "this"),
+            Self::True => write!(f, "true"),
+            Self::Var => write!(f, "var"),
+            Self::While => write!(f, "while"),
+            Self::Eof => write!(f, "eof"),
+        }
+    }
 }
