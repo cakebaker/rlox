497c0692a7f3ccbd962e658905bb6e3e5cae05e2
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2021-06-05 16:12:13 +0200

    Implement fmt::Display for RuntimeError

--- src/runtime_error.rs
+++ src/runtime_error.rs
@@ -1,4 +1,6 @@
 use crate::value::Value;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum RuntimeError {
@@ -7,4 +9,18 @@ pub enum RuntimeError {
     Return(Value),
     UndefinedVariable(String),
     ValueNotCallable(Value),
+}
+
+impl Error for RuntimeError {}
+
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidOperator => write!(f, "Invalid operator"),
+            Self::InvalidType => write!(f, "Invalid type"),
+            Self::Return(value) => write!(f, "{}", value),
+            Self::UndefinedVariable(var) => write!(f, "Undefined variable: '{}'", var),
+            Self::ValueNotCallable(value) => write!(f, "Value not callable: '{}'", value),
+        }
+    }
 }
